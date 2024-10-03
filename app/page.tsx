f594bbf0f0395c3956cfc5215b1942e81beb8a9c
vandilsonbrito <vandilson25@outlook.com>
2024-10-03 17:09:20 -0300

    Feat: add header, banner, services, whyus, testemonial and footer components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
+import Banner from "./components/Banner";
+import Testemonial from "./components/Testemonial";
+import Header from "./components/Header";
+import Services from "./components/Services";
+import WhyUs from "./components/WhyUs";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <main className="w-full h-full">
-        
+    <main className="w-full h-full" id="top">
+        <Header/>
+        <div className="w-full h-full flex flex-col justify-center items-center">
+          <Banner/>
+          <Services/>
+          <WhyUs/>
+          <Testemonial/>
+        </div>
+        <Footer/>
     </main>
   );
 }
