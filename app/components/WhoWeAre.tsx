ebf7ce50decad67867e2400946f9ee02be8901cb
vandilsonbrito <vandilson25@outlook.com>
2024-10-05 08:13:55 -0300

    Feat: create WhoWeAre component

---
+++ app/components/WhoWeAre.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function WhoWeAre() {
+  return (
+    <section className="w-full max-w-6xl h-full flex flex-col-reverse lg:flex-row items-start px-8 lg:px-0 py-10 lg:py-12 text-black " id="who-we-are">
+        <div className="w-full h-full mt-7 lg:mt-0">
+            <div className="w-full h-full flex justify-center md:items-center lg:items-start ">
+                <div className="w-full md:w-[24rem] lg:w-full h-[13rem] md:h-[16rem] lg:h-[21rem] bg-whoWeAreImg bg-no-repeat bg-contain"></div>
+            </div>
+        </div>
+        <div className="w-full h-full flex flex-col gap-4">
+            <h2 className='w-full text-3xl font-semibold text-left -mt-[0.45rem]'>Quem Somos</h2>
+            <p className='font-medium'>Nossa clínica nasceu com o propósito de oferecer um atendimento médico de qualidade, baseado em um relacionamento próximo e acolhedor com nossos pacientes. Com uma equipe de <strong>especialistas altamente qualificados</strong> e <strong>dedicados ao bem-estar de cada pessoa</strong>, estamos comprometidos em proporcionar um atendimento <strong>humanizado</strong> e <strong>personalizado</strong>, focado nas necessidades específicas de cada paciente.</p>
+
+            <div className="w-full h-full flex flex-col gap-2 font-medium">
+                <h3 className="text-lg font-semibold">Nossa Missão</h3>
+                <p>Prover cuidados de saúde excepcionais com ética, profissionalismo e empatia, promovendo a saúde e o bem-estar de todos que confiam em nossos serviços.</p>
+            </div>
+
+        </div>
+    </section>
+  )
+}
