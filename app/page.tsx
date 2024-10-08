fef50a1bb05c2503e665dbd79cbb6578bd84f63f
vandilsonbrito <vandilson25@outlook.com>
2024-10-08 14:08:06 -0300

    Feat: add 'ScrollToTop' and 'WhoWeAre' components

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,17 @@ import Header from "./components/Header";
 import Services from "./components/Services";
 import WhyUs from "./components/WhyUs";
 import Footer from "./components/Footer";
+import WhoWeAre from "./components/WhoWeAre";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function Home() {
   return (
     <main className="w-full h-full" id="top">
         <Header/>
         <div className="w-full h-full flex flex-col justify-center items-center">
+          <ScrollToTop/>
           <Banner/>
+          <WhoWeAre/>
           <Services/>
           <WhyUs/>
           <Testemonial/>
