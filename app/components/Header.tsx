589a0cf3d049e4e20f25dbba31205f222b81a891
vandilsonbrito <vandilson25@outlook.com>
2024-10-08 23:11:56 -0300

    Feat: change image tag

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,13 +3,13 @@ import { Button } from '@/components/ui/button';
 import { Link } from 'react-scroll';
 import React from 'react';
 import Logo from '../../public/logo-clinica-saude.png'
+import Image from 'next/image';
 
 export default function Header() {
   return (
     <header className='w-full h-[4.5rem] flex justify-center items-center font-medium px-5 md:px-14 bg-white shadow-lg sticky top-0 z-50' >
-        {/* eslint-disable-next-line @next/next/no-img-element */}
         <div className="w-full h-full max-w-7xl flex justify-between items-center">
-          <img src={Logo.src} alt="Logo" className='w-[5.5rem]'/>
+          <Image src={Logo.src} alt="Logo" className='w-[5.5rem]' width={88} height={88}/>
           <nav className='w-full max-w-[35rem]'>
               <ul className='w-full flex items-center justify-end lg:justify-between'>
                   <li className="cursor-pointer hidden lg:block">
