efe1e0cfcfe75e2bd995e9343b9dbcf4d278a41d
vandilsonbrito <vandilson25@outlook.com>
2024-10-13 21:57:34 -0300

    Feat: create btnLogin component

---
+++ app/components/BtnLogIn.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from '../../components/ui/button';
+import Link from 'next/link';
+
+export default function BtnLogIn() {
+  return (
+    <li>
+        <Button asChild className="bg-bluePrimary hover:bg-blueSecundary w-28 font-medium">
+            <Link href="/sign-in">Login</Link>
+        </Button>
+    </li>
+  )
+}
