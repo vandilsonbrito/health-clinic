2a08e7bce6cddb8eb41de1cd4171c2cd04ff98cb
vandilsonbrito <vandilson25@outlook.com>
2024-10-14 20:34:23 -0300

    Feat: add btnLogin component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,32 +1,53 @@
 'use client';
-import { Button } from '../../components/ui/button';
+//import { Button } from '@/components/ui/button';
 import { Link } from 'react-scroll';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Logo from '../../public/logo-clinica-saude.png';
 import Image from 'next/image';
+import BtnLogIn from './BtnLogIn';
+import { useAuth } from '../../firebase/authContext';
+
 
 export default function Header() {
-  return (
-    <header className='w-full h-[4.5rem] flex justify-center items-center font-medium px-5 md:px-14 bg-white shadow-lg sticky top-0 z-50' >
-        <div className="w-full h-full max-w-7xl flex justify-between items-center">
-          <Image src={Logo} alt="Logo" className='w-[5.5rem]' width={200} height={200}/>
-          <nav className='w-full max-w-[35rem]'>
-              <ul className='w-full flex items-center justify-end lg:justify-between'>
-                  <li className="cursor-pointer hidden lg:block">
-                    <Link to="top" smooth={true} duration={300}>Home</Link >
-                  </li>
-                  <li className="cursor-pointer hidden lg:block">
-                    <Link to="who-we-are" smooth={true} duration={300} offset={-100}>Quem Somos</Link >
-                  </li>
-                  <li className="cursor-pointer hidden lg:block">
-                    <Link to="especialidades" smooth={true} duration={300} offset={-50}>Especialidades</Link >
-                  </li>
-                  <li>
-                      <Button className="bg-bluePrimary w-28 font-medium">Login</Button>
-                  </li>
-              </ul>
-          </nav>
-        </div>
-    </header>
-  )
+
+    /* const router = useRouter(); */
+    const { userAuth, login, logout } = useAuth();
+
+    useEffect(() => {
+      console.log('UserAuth', userAuth);
+    }, [userAuth, login, logout]);
+
+    return (
+      <header className='w-full h-[4.5rem] flex justify-center items-center font-medium px-5 md:px-14 bg-white shadow-lg sticky top-0 z-50' >
+          <div className="w-full h-full max-w-7xl flex justify-between items-center">
+            <Image src={Logo} alt="Logo" className='w-[5.5rem]' width={200} height={200}/>
+
+            {
+              userAuth ?
+              (
+                <nav className=''>
+                </nav>
+              )
+              :
+              (
+                <nav className='w-full max-w-[35rem]'>
+                    <ul className='w-full flex items-center justify-end lg:justify-between'>
+                        <li className="cursor-pointer hidden lg:block">
+                          <Link to="top" smooth={true} duration={300}>Home</Link >
+                        </li>
+                        <li className="cursor-pointer hidden lg:block">
+                          <Link to="who-we-are" smooth={true} duration={300} offset={-100}>Quem Somos</Link >
+                        </li>
+                        <li className="cursor-pointer hidden lg:block">
+                          <Link to="especialidades" smooth={true} duration={300} offset={-50}>Especialidades</Link >
+                        </li>
+                        <BtnLogIn/>
+                    </ul>
+                </nav>
+              )
+            }
+            
+          </div>
+      </header>
+    )
 }
