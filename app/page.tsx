63028da6d5da344bc0c5014cbe3d2329fc76dbb6
vandilsonbrito <vandilson25@outlook.com>
2024-10-15 19:16:18 -0300

    Feat: add header and footer components

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,25 @@
 import Banner from "./components/Banner";
 import Testemonial from "./components/Testemonial";
-import Header from "./components/Header";
 import Services from "./components/Services";
 import WhyUs from "./components/WhyUs";
-import Footer from "./components/Footer";
 import WhoWeAre from "./components/WhoWeAre";
 import ScrollToTop from "./components/ScrollToTop";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <main className="w-full h-full" id="top">
-        <Header/>
-        <div className="w-full h-full flex flex-col justify-center items-center">
+    <>
+      <Header/>
+      <main className="w-full h-full flex flex-col justify-center items-center" id="top">
           <ScrollToTop/>
           <Banner/>
           <WhoWeAre/>
           <Services/>
           <WhyUs/>
           <Testemonial/>
-        </div>
-        <Footer/>
-    </main>
+      </main>
+      <Footer/>
+    </>
   );
 }
