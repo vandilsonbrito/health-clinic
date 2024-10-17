3a104718ef483fb57a8610964099eba1d745dbd2
vandilsonbrito <vandilson25@outlook.com>
2024-10-17 00:18:04 -0300

    Feat: add professional data to global storage

--- app/components/Appointment/CarouselAppointContainer.tsx
+++ app/components/Appointment/CarouselAppointContainer.tsx
@@ -1,19 +1,38 @@
+import useGlobalStore from '@/utils/globalStorage';
 import Image, { StaticImageData } from 'next/image'
 import React from 'react'
 
-export default function CarouselAppointContainer({ url, title }: { url: StaticImageData, title: string }) {
-  return (
-    <button className="w-40 p-2 py-5 rounded-3xl flex flex-col justify-center items-center gap-1 bg-blue-50 hover:bg-blue-100 transform ease-in-out duration-200 text-black text-4xl border">
-        <div className="flex flex-col items-center justify-center rounded-full bg-white ">
-            <Image 
-              className='rounded-full'
-              src={url} 
-              alt='Profissional Image' 
-              width={100} 
-              height={100}
-              />
-        </div>
-        <h3 className='text-[1rem] font-medium '>{title}</h3>    
-    </button> 
-  )
+export default function CarouselAppointContainer({ url, especiality, professionalName }: { url: StaticImageData, especiality: string, professionalName: string }) {
+
+    const { addEspeciality, removeEspeciality, selectedEspeciality } = useGlobalStore();
+
+    const handleChooseProfessional = () => {
+      if(selectedEspeciality.length > 0){
+        selectedEspeciality.forEach((item) => {
+          removeEspeciality([item])
+        })
+      }
+      addEspeciality([professionalName, especiality])
+    }
+  
+    return (
+        <button 
+          onClick={() => handleChooseProfessional()}
+          className="w-40 p-2 py-5 rounded-3xl flex flex-col justify-center items-center gap-1 bg-[#f9fbff] hover:bg-blue-100 transform ease-in-out duration-200 text-black text-4xl border"
+          >
+            <div className="flex flex-col items-center justify-center rounded-full bg-white ">
+                <Image 
+                  className='rounded-full'
+                  src={url} 
+                  alt='Profissional Image' 
+                  width={100} 
+                  height={100}
+                  />
+            </div>
+            <div className="flex flex-col gap-1">
+              <p className='text-sm font-medium '>{professionalName}</p>
+              <h3 className='text-[1rem] font-medium '>{especiality}</h3>
+            </div>    
+        </button> 
+    )
 }
