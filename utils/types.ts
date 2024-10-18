68cc517996a2248ec5e9014fc50dad0fa2b38ab9
vandilsonbrito <vandilson25@outlook.com>
2024-10-18 11:24:32 -0300

    Chore: add 'AuthContextType', 'ServicesDataType ' and 'ProfessionalData ' interfaces

--- utils/types.ts
+++ utils/types.ts
@@ -1,3 +1,12 @@
+import { User } from "firebase/auth";
+
+export interface AuthContextType {
+    userAuth: User | null;
+    login: (email: string, password: string) => Promise<void>;
+    logout: () => Promise<void>;
+    authLoading: boolean; 
+}
+
 export interface ProfessionalData {
     name: string;
     especiality: string;
@@ -16,3 +25,19 @@ export interface selectedConsultationType {
 export interface SectionsObjType {
     [key: number]: React.ReactElement
 } 
+
+export interface ServicesDataType {
+    especiality: ProfessionalData
+}
+export interface ProfessionalData {
+    [key: number]: {
+        professionalName: {
+            especiality: string,
+            name: string,
+            agenda: {
+                date: string[],
+                time: string[]
+            }
+        }
+    }
+}
