faed0eaf185f399ba77079a7f4ac1b5eda42b70f
vandilsonbrito <vandilson25@outlook.com>
2024-10-18 11:32:35 -0300

    Feat: display especialists based on data from database

--- app/components/Appointment/AvailableSpecialities.tsx
+++ app/components/Appointment/AvailableSpecialities.tsx
@@ -1,12 +1,45 @@
 'use client';
-import React from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import CarouselAppointContainer from './CarouselAppointContainer';
 import man2 from '../../../public/man2.jpg';
+import { getData } from '@/firebase/databaseFunc';
+import { ProfessionalData, ServicesDataType } from '@/utils/types';
 
 export default function AvailableSpecialities() {
+
+    const [servicesData, setServicesData] = useState<ProfessionalData[] | null>(null);
+    const [especiality, setEspeciality] = useState<string[]>([]);
+    const [professionalsName, setProfessionalsName] = useState<string[]>([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const data = await getData({route: 'services/consultation'});
+            if(data) {
+                let auxArr: string[] = [], auxArr2: string[] = [];
+            
+                console.log("Data", data)
+
+                for(const especiality in data) {
+                    for(const professional in data[especiality]) {
+                        auxArr.push(data[especiality][professional].especiality);
+                        auxArr2.push(data[especiality][professional].name);
+                    }
+                }
+
+                setEspeciality(auxArr);   
+                setProfessionalsName(auxArr2);
+            }
+        }  
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        console.log("especiality", especiality);
+        console.log("professionalsName", professionalsName);
+    }, [especiality, professionalsName])
 
     const settings = {
         dots: true,
@@ -23,20 +56,26 @@ export default function AvailableSpecialities() {
         <div className="flex flex-col gap-4">
             <h2 className='font-medium'>Especialidades Disponíveis</h2>
             <div className="slider-container p-3 pb-0 gap-2">
-                <Slider {...settings}>
-                    <CarouselAppointContainer url={man2} especiality={'Obstetrícia'} professionalName={'Rodrigo Peixoto'}/>
-                    <CarouselAppointContainer url={man2} especiality={'Odontologia'} professionalName={'Lucas Mendes'}/>
-                    <CarouselAppointContainer url={man2} especiality={'Cardiologia'} professionalName={'Thiago Silva'}/>
-                    <CarouselAppointContainer url={man2} especiality={'Oftalmologia'} professionalName={'Marina Castro'}/>
-                    <CarouselAppointContainer url={man2} especiality={'Odontopediatria'} professionalName={'Julia Lemos'}/>
-                    <CarouselAppointContainer url={man2} especiality={'Nutrologia'} professionalName={'Mateus Bonfim'}/>
-                    <CarouselAppointContainer url={man2} especiality={'Fonoaudiologia'} professionalName={'Cristina Fernandes'}/>
-                    <CarouselAppointContainer url={man2} especiality={'Endocrinologia'} professionalName={'João Pedro Lima'}/>
-                    <CarouselAppointContainer url={man2} especiality={'Psicologia'} professionalName={'Bruna Souza'}/>
-                    <CarouselAppointContainer url={man2} especiality={'Pediatria'} professionalName={'Letícia Carvalho'}/>
-                    <CarouselAppointContainer url={man2} especiality={'Dermatologia'} professionalName={'Luana Peixoto'}/>
-                    <CarouselAppointContainer url={man2} especiality={'Ortopedia'} professionalName={'Marcos Trindade'}/>
-                </Slider>
+                <Suspense 
+                    fallback={
+                    <div className="w-full h-full min-h-[30rem] flex flex-col justify-center items-center">
+                        <p className='text-lg mb-4'>Loading</p>
+                        <span className="loader"></span>
+                    </div> 
+                    }>   
+
+                        <Slider {...settings}>
+                            {
+                                (especiality)?.map((item, index) => {
+                                    return (
+                                    <CarouselAppointContainer key={index} url={man2} especiality={item} professionalName={professionalsName[index]}/>
+                                    )
+                                })
+                                
+                            }
+                            
+                        </Slider>
+                </Suspense>
             </div>
         </div>
     )
