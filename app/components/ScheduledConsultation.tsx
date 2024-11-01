a7267b20c22b3cebfc9a32b8fe766cd2f38313d1
vandilsonbrito <vandilson25@outlook.com>
2024-11-01 01:01:24 -0300

    Feat: add logic to display scheduled appointements from db as a table

--- app/components/ScheduledConsultation.tsx
+++ app/components/ScheduledConsultation.tsx
@@ -1,38 +1,59 @@
-import React, { ReactElement, useEffect, useState } from 'react';
+import React, { ReactElement, Suspense, useEffect, useState } from 'react';
 import { FaCalendarCheck } from 'react-icons/fa';
 import CancelAppointmentModal from './CancelAppointmentModal';
 import { Button } from '@/components/ui/button';
 import useGlobalStore from '@/utils/globalStorage';
+import { useGetAppointmentsDataFromDB } from '@/firebase/databaseFunc';
+import { useAuth } from '@/firebase/authContext';
+import { AppointmentFormatType } from '@/utils/types';
 
 export default function ScheduledConsultation() {
 
-    // Fazer consulta para consultas agendadas route 'users/appointments'
-    const appointment = [{
-        profissional: 'Luana Lima',
-        especialidade: 'Nutrologia',
-        dataAppointment: '29/11/2024 - 07:00'
-    }];
+    const { userAuth } = useAuth();
+    const [userId, setUserId] = useState<string>('');
+    const [appointmentsFromDB, setAppointmentsFromDB] = useState<AppointmentFormatType[]>([]);
+    useEffect(() => {
+        if(userAuth && userAuth.uid) {
+            setUserId(userAuth.uid)
+        } 
+    }, [userAuth?.uid])
+    const { data: userAppointmentData } = useGetAppointmentsDataFromDB({ route: `users/${userAuth?.uid}/appointments`, userID: userId });
+    
+    useEffect(() => {
+        const arrAux = []
+        for(const appointmentIDs in userAppointmentData) {
+            arrAux.push(userAppointmentData[appointmentIDs][0])
+        }
+        setAppointmentsFromDB(arrAux)
+        console.log("userAppointmentData", userAppointmentData)
+    }, [userAppointmentData]);
 
-    const [appointments, setAppointments] = useState<ReactElement[]>();
+    useEffect(() => {
+        console.log("appointmentsFromDB", appointmentsFromDB)
+    }, [appointmentsFromDB])
+
+
+    const [appointmentsNode, setAppointmentsNode] = useState<ReactElement[]>();
     const { setIsCancelAppointmentModalOpen } = useGlobalStore();
 
     useEffect(() => {
-        const auxArr: ReactElement[] = []; 
-        const displayAppointments = () => {
-            appointment.map((item) => {
-                auxArr.push(
-                    <tr>
-                        <td>{item.dataAppointment}</td>
-                        <td className='uppercase'>{item.especialidade}</td>
-                        <td>{item.profissional}</td>
-                    </tr>
-                )
-            });
-            setAppointments(auxArr);
+        if(appointmentsFromDB.length > 0) {
+            const auxArr: ReactElement[] = []; 
+            const displayAppointments = () => {
+                appointmentsFromDB.map((item: AppointmentFormatType, index: number) => {
+                    auxArr.push(
+                        <tr key={index} className="w-full uppercase">
+                            <td>{`${item?.date} - ${item?.time}`}</td>
+                            <td>{item?.especiality}</td>
+                            <td>{item?.professionalName}</td>
+                        </tr>
+                    )
+                });
+                setAppointmentsNode(auxArr);
+            }
+            displayAppointments();
         }
-
-        displayAppointments();
-    }, [])
+    }, [appointmentsFromDB])
 
     return (
         <div className='w-full h-full'>
@@ -43,16 +64,31 @@ export default function ScheduledConsultation() {
 
             <div className="w-full h-full border flex flex-col gap-4  mt-5 p-2">
                 <h2 className='font-medium'>Próxima(s) Consultas</h2>
-                <table className='w-full'>
-                    <tbody>
-                        <tr className='bg-[#e9f1f8da]'>
-                            <td>Data/Hora</td>
-                            <td className='uppercase'>Especialidade</td>
-                            <td>Profissional</td>
-                        </tr>
-                        {appointments}
-                    </tbody>
-                </table>
+                <Suspense fallback={
+                    <div className="w-full h-full min-h-[13rem] flex flex-col justify-center items-center">
+                            <p className='text-lg mb-4'>Loading</p>
+                            <span className="loader"></span>
+                        </div> } >
+                    {
+                        appointmentsFromDB.length > 0 &&
+                        (
+                            <div className="w-full h-full max-h-[calc(100vh-300px)] overflow-y-auto">
+                                <table className='w-full h-full'>
+                                    <thead>
+                                        <tr className='bg-[#e9f1f8da]'>
+                                            <td className='uppercase text-center'>Data/Hora</td>
+                                            <td className='uppercase text-center'>Especialidade</td>
+                                            <td className='uppercase text-center'>Profissional</td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {appointmentsNode}  
+                                    </tbody>
+                                </table>
+                            </div>
+                        )
+                    }
+                </Suspense>
                 <div className='w-full h-full flex justify-center items-center mt-2'>
                     <Button
                         onClick={() => setIsCancelAppointmentModalOpen(true)}
