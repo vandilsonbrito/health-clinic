e308a081e5d526e17422b097f03254365600233b
vandilsonbrito <vandilson25@outlook.com>
2024-11-01 00:28:47 -0300

    Feat: display available especialists from db, save the chosen one on 'selectedEspeciality' state and set 'jumpToNextStep'

--- app/components/Appointment/AvailableSpecialities.tsx
+++ app/components/Appointment/AvailableSpecialities.tsx
@@ -3,16 +3,17 @@ import React, { useEffect, useState } from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import CarouselAppointContainer from './CarouselAppointContainer';
+import CarouselSpecialistsContainer from './CarouselSpecialistsContainer';
 import man2 from '../../../public/man2.jpg';
-import { getData } from '@/firebase/databaseFunc';
+import { useDataFromDB } from '@/firebase/databaseFunc';
 //import { ProfessionalData } from '@/utils/types';
 import useMedia from 'use-media';
 import useGlobalStore from '@/utils/globalStorage';
 
 export default function AvailableSpecialities() {
 
-    const { setJumpToNextStep } = useGlobalStore();
+    const { data: specialistsData } = useDataFromDB({route: 'services/consultation'});
+    const { setJumpToScheduleAppointmentNextStep } = useGlobalStore();
     //const [servicesData, setServicesData] = useState<ProfessionalData[] | null>(null);
     const [especiality, setEspeciality] = useState<string[]>([]);
     const [professionalsName, setProfessionalsName] = useState<string[]>([]);
@@ -21,17 +22,16 @@ export default function AvailableSpecialities() {
     const isMediumMobile = useMedia({maxWidth: '1020px'});
 
     useEffect(() => {
-        async function fetchData() {
-            const data = await getData({route: 'services/consultation'});
-            if(data) {
+        function fetchData() {
+            if(specialistsData) {
                 const auxArr: string[] = [], auxArr2: string[] = [];
             
-                console.log("Data", data)
+                console.log("Data", specialistsData)
 
-                for(const especiality in data) {
-                    for(const professional in data[especiality]) {
-                        auxArr.push(data[especiality][professional].especiality);
-                        auxArr2.push(data[especiality][professional].name);
+                for(const especiality in specialistsData) {
+                    for(const professional in specialistsData[especiality]) {
+                        auxArr.push(specialistsData[especiality][professional].especiality);
+                        auxArr2.push(specialistsData[especiality][professional].name);
                     }
                 }
 
@@ -41,13 +41,8 @@ export default function AvailableSpecialities() {
         }  
         fetchData();
 
-        setJumpToNextStep(false);
-    }, []);
-
-    /* useEffect(() => {
-        console.log("especiality", especiality);
-        console.log("professionalsName", professionalsName);
-    }, [especiality, professionalsName]) */
+        setJumpToScheduleAppointmentNextStep(false);
+    }, [specialistsData, setJumpToScheduleAppointmentNextStep]);
 
     const settings = {
         dots: true,
@@ -61,9 +56,12 @@ export default function AvailableSpecialities() {
     };
     
     return (
-        <div className="flex flex-col gap-4">
-            <h2 className='font-medium'>Especialidades Disponíveis</h2>
-            <div className="slider-container px-0 py-3 sm:p-3 pb-0 gap-2">
+        <div className="flex flex-col gap-2">
+            <div>
+                <h2 className='font-medium'>Especialidades Disponíveis</h2>
+                <p className='text-[.7rem]'>* Selecione uma especialidade</p>
+            </div>
+            <div className="slider-container px-0 py-3 sm:px-3 pb-0 gap-2">
                 {
                     especiality && especiality.length > 0 ?
                     (
@@ -71,7 +69,7 @@ export default function AvailableSpecialities() {
                             {
                                 especiality?.map((item, index) => {
                                     return (
-                                    <CarouselAppointContainer key={index} url={man2} especiality={item} professionalName={professionalsName[index]}/>
+                                    <CarouselSpecialistsContainer key={index} url={man2} especiality={item} professionalName={professionalsName[index]}/>
                                     )
                                 })
                             }             
