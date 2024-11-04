3d0a43711f1ed40e5c6e113a02920685bfd8e834
vandilsonbrito <vandilson25@outlook.com>
2024-11-04 16:36:34 -0300

    Feat: add 'isLoading' prop from react-query and add data loading fallback

--- app/components/ScheduledConsultation.tsx
+++ app/components/ScheduledConsultation.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { ReactElement, Suspense, useEffect, useState } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import { FaCalendarCheck } from 'react-icons/fa';
 import { useGetAppointmentsDataFromDB } from '@/firebase/databaseCRUDFunctions';
 import { useAuth } from '@/firebase/authContext';
@@ -22,7 +22,7 @@ export default function ScheduledConsultation() {
             setUserId(userAuth.uid)
         } 
     }, [userAuth])
-    const { data: userAppointmentData, refetch} = useGetAppointmentsDataFromDB({ route: `users/${userAuth?.uid}/appointments`, userID: userId });
+    const { data: userAppointmentData, refetch, isLoading } = useGetAppointmentsDataFromDB({ route: `users/${userAuth?.uid}/appointments`, userID: userId });
 
     useEffect(() => {
         if(!respondedValueModalAppointmentCancel) return;
@@ -98,38 +98,41 @@ export default function ScheduledConsultation() {
             <div className="w-full h-full border flex flex-col gap-4  mt-5 p-2">
                 <h2 className='font-medium'>Próxima(s) Consultas</h2>
                 { showDeleteAppointmentMessage && <p className='text-[.7rem]'>*Selecione Data/Horário para cancelar</p> }
-                <Suspense fallback={
+               
+                {   
+                    isLoading ? 
+                    (
                         <div className="w-full h-full min-h-[13rem] flex flex-col justify-center items-center">
                             <p className='text-lg mb-4'>Loading</p>
                             <span className="loader"></span>
                         </div> 
-                    } >
-                    {
+                    )
+                    :
                         appointmentsFromDB.length > 0 ?
-                        (
-                            <div className="w-full h-full max-h-[calc(100vh-300px)] overflow-y-auto">
-                                <table className='w-full h-full'>
-                                    <thead>
-                                        <tr className='bg-[#e9f1f8da] font-semibold uppercase'>
-                                            <td className='text-center'>Data/Hora</td>
-                                            <td className='text-center'>Profissional</td>
-                                            <td className='text-center'>Especialidade</td>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {appointmentsNode}  
-                                    </tbody>
-                                </table>
-                            </div>
-                        )
-                        :
-                        (
-                            <div className="w-full h-full min-h-[13rem] flex flex-col justify-center items-center">
-                                <p className=''>Você ainda não tem consultas agendadas.</p>
-                            </div>
-                        )
-                    }
-                </Suspense>
+                            (
+                                <div className="w-full h-full max-h-[calc(100vh-333px)] overflow-y-auto">
+                                    <table className='w-full h-full'>
+                                        <thead className='bg-[#e9f1f8da] font-semibold uppercase'>
+                                            <tr>
+                                                <td className='text-center'>Data/Hora</td>
+                                                <td className='text-center'>Profissional</td>
+                                                <td className='text-center'>Especialidade</td>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {appointmentsNode}  
+                                        </tbody>
+                                    </table>
+                                </div>
+                            )
+                            :
+                            (
+                                <div className="w-full h-full min-h-[13rem] flex flex-col justify-center items-center">
+                                    <p className=''>Você ainda não tem consultas agendadas.</p>
+                                </div>
+                            )    
+                }
+                
                 <div className={appointmentsFromDB.length > 0 ? `w-full h-full flex justify-center items-center mt-2` : 'hidden'}>
                     <CancelAppointment appointmentId={removeSelectedAppointmentID} userId={userId}
                     />
