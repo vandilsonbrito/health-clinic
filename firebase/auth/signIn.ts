37459ebfea87f9200c6582bfebe45325b0f3d404
vandilsonbrito <vandilson25@outlook.com>
2024-11-06 23:21:07 -0300

    Chore: remove rextern try/catch

--- firebase/auth/signIn.ts
+++ firebase/auth/signIn.ts
@@ -4,28 +4,26 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 export default async function SignIn(email: string, password: string) {
     let userCredential = null,
         error = null;
-    try {
-        try {
-            userCredential = await signInWithEmailAndPassword(auth, email, password);
-            const token = await userCredential.user.getIdToken();
-    
-            // Envie o token para seu backend para verificação
-            const response = await fetch('/api/auth', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${token}`,
-                },
-            });
-            const data = await response.json();
-            console.log(data);
-        } catch (error) {
-            console.error('Login error', error);
-        }
-    }
-    catch(Error) {
-        error = Error;
-    }
 
+    try {
+        userCredential = await signInWithEmailAndPassword(auth, email, password);
+        const token = await userCredential.user.getIdToken();
+
+        // Envie o token para seu backend para verificação
+        const response = await fetch('/api/auth', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        const data = await response.json();
+        console.log(data);
+    } 
+    catch (e) {
+        error = e;
+        console.error('Login error', error);
+    }
+   
     return { userCredential, error };
 }
