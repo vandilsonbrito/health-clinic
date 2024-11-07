642c0aa41330f6762cef345feb651b34877c089d
vandilsonbrito <vandilson25@outlook.com>
2024-11-06 23:23:13 -0300

    Chore: remove name funct prop and code to set user info to db

--- firebase/auth/signUp.ts
+++ firebase/auth/signUp.ts
@@ -1,21 +1,11 @@
 import { auth } from "../firebaseAuthConfig";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { firestore } from "../firebaseDBConfig";
-import { collection, addDoc } from "firebase/firestore";
 
-export default async function SignUp(name: string, email: string, password: string) {
+export default async function SignUp(email: string, password: string) {
     let result = null,
         error = null;
     try {
         result = await createUserWithEmailAndPassword(auth, email, password);
-
-        const userCollectionRef = collection(firestore, 'users');
-
-        await addDoc(userCollectionRef, {
-            username: name,
-            email: email
-        })
-        
     }
     catch(e) {
         error = e;
