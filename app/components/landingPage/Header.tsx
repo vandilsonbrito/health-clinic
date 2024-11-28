2f09a09525317be77e87abbe11714e71db1e348a
vandilsonbrito <vandilson25@outlook.com>
2024-11-28 17:33:04 -0300

    feat: add burguer menu for mobile on landing page

--- app/components/landingPage/Header.tsx
+++ app/components/landingPage/Header.tsx
@@ -7,10 +7,14 @@ import Image from 'next/image';
 import BtnLogIn from './BtnLogIn';
 import { useAuth } from '../../../firebase/authContext';
 import { Button } from '@/components/ui/button';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { MenuIcon } from 'lucide-react';
+import { useRouter } from "next/navigation";
 
 export default function Header() {
 
     const { userAuth, logout } = useAuth();
+    const router = useRouter();
 
     return (
       <header className='w-full h-[4.5rem] flex justify-center items-center font-medium px-5 md:px-14 bg-white shadow-lg sticky top-0 z-50' >
@@ -26,10 +30,40 @@ export default function Header() {
             </Link>
             {
               userAuth ?
-              (
-                <Button className="bg-bluePrimary hover:bg-blueSecundary w-28 font-medium active:scale-x-[.98]" onClick={() => logout()}>
-                    Logout
-                </Button>
+              ( 
+                <>
+                  <nav className="lg:hidden">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger>
+                            <div className="rounded-xl border px-2 py-1 flex items-center gap-x-3">
+                                <MenuIcon className="text-blueSecundary w-6 h-6 lg:w-5 lg:h-5"/>
+                            </div>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="center" className="w-[200px]">
+                            <DropdownMenuItem>
+                                <Button 
+                                    onClick={() => router.push('/agendamento')}
+                                    className='bg-transparent hover:bg-transparent text-black active:scale-x-[.98]'>
+                                    Marcar Consulta
+                                </Button>
+                            </DropdownMenuItem> 
+                            <DropdownMenuSeparator/>
+
+                            <DropdownMenuItem>
+                                <Button asChild className='bg-bluePrimary  active:scale-x-[.98]'>
+                                    <Link href="" className="w-full" onClick={() => logout()}>Log out</Link>
+                                </Button>          
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+
+                    </DropdownMenu>  
+                  </nav>
+                  <nav className="hidden lg:block">
+                    <Button asChild className='px-7 max-w-28 bg-bluePrimary hover:bg-blueSecundary active:scale-x-[.98]'>
+                          <Link href="" className="w-full" onClick={() => logout()}>Sair</Link>
+                      </Button>  
+                  </nav>
+                </>
               )
               :
               (
