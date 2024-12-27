59edc46c1bb82bd031ccfe2d92ca731aa31fb914
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2024-12-27 15:19:35 -0300

    feat: create AppointmentInfo component

---
+++ app/components/Admin/Dashboard/AppointmentInfo/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <div className='w-full h-full border-slate-200 border-b-[1px] flex items-center justify-between'>
+        <div className="flex flex-col">
+            <p className='text-[.8rem] text-slate-600 font-medium'>Marcos Augusto</p>   
+            <p className='text-[.7rem] text-slate-500'>Oftalmologista</p>
+        </div>
+        <div className="flex flex-col">
+            <p className='text-[.7rem] text-slate-500'>Hoje</p>
+            <p className='text-[.78rem] font-medium text-slate-600'>15:00</p>
+        </div>
+    </div>
+  )
+}
