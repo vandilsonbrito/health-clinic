1ba99ffda618bad77289dee768f0a03d3f22a3c1
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-04-04 10:04:59 -0300

    feat: add db integration and error handling

--- app/admin/pacientes-cadastrados/page.tsx
+++ app/admin/pacientes-cadastrados/page.tsx
@@ -1,45 +1,39 @@
 'use client'
-import { useState } from "react"
+import { useMemo, useState } from "react"
 import { Input } from "@/components/ui/input"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import Layout from "@/app/components/Admin/Layout"
+import { useDataFromDB } from "@/firebase/firebaseDBServices"
+import { format, parseISO } from "date-fns"
 
-const patients = [
-  {
-    name: "João Silva",
-    email: "joao.silva@example.com",
-    phone: "(11) 98765-4321",
-    cpf: "123.456.789-00",
-    lastAppointment: "2024-03-10",
-    totalAppointments: 5,
-  },
-  {
-    name: "Ana Oliveira",
-    email: "ana.oliveira@example.com",
-    phone: "(11) 97654-3210",
-    cpf: "987.654.321-00",
-    lastAppointment: "2024-02-25",
-    totalAppointments: 3,
-  },
-  {
-    name: "Pedro Costa",
-    email: "pedro.costa@example.com",
-    phone: "(11) 96543-2109",
-    cpf: "456.789.123-00",
-    lastAppointment: "2024-03-05",
-    totalAppointments: 7,
-  },
-]
+interface Patient {
+  profile: {
+    name: string
+    email: string
+    cellphone: string
+    cpf: string
+    lastAppointmentDate: string
+    totalOfAppointments: number
+  }
+}
+
 
 export default function Patients() {
+
+  const { data, isLoading, isError } = useDataFromDB({route: 'users', queryKey: 'registered-patients-data' })
   const [search, setSearch] = useState("")
 
-  const filteredPatients = patients.filter(
+  const registeredPatientsData = useMemo(() => {
+    if (!data) return [];
+    return Object.entries(data);
+  }, [data]);
+
+  const filteredPatients = registeredPatientsData?.filter(
     (patient) =>
-      patient.name.toLowerCase().includes(search.toLowerCase()) ||
-      patient.email.toLowerCase().includes(search.toLowerCase()) ||
-      patient.cpf.includes(search),
-  )
+      (patient[1] as Patient).profile?.name?.toLowerCase().includes(search.toLowerCase()) ||
+      (patient[1] as Patient).profile?.email?.toLowerCase().includes(search.toLowerCase()) ||
+      (patient[1] as Patient).profile?.cpf?.includes(search),
+  )  
 
   return (
     <Layout>
@@ -53,30 +47,41 @@ export default function Patients() {
             onChange={(e) => setSearch(e.target.value)}
           />
         </div>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Nome</TableHead>
-              <TableHead>Email</TableHead>
-              <TableHead>Telefone</TableHead>
-              <TableHead>CPF</TableHead>
-              <TableHead>Última Consulta</TableHead>
-              <TableHead>Total de Consultas</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {filteredPatients.map((patient, index) => (
-              <TableRow key={index}>
-                <TableCell>{patient.name}</TableCell>
-                <TableCell>{patient.email}</TableCell>
-                <TableCell>{patient.phone}</TableCell>
-                <TableCell>{patient.cpf}</TableCell>
-                <TableCell>{patient.lastAppointment}</TableCell>
-                <TableCell>{patient.totalAppointments}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+          { isLoading && <span className="mt-5">Carregando...</span> }
+          { isError && <span className="mt-5">Erro ao carregar dados.</span> }
+          { !isLoading && !isError && (
+            <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Nome</TableHead>
+                    <TableHead>Email</TableHead>
+                    <TableHead>Telefone</TableHead>
+                    <TableHead>CPF</TableHead>
+                    <TableHead>Última Consulta</TableHead>
+                    <TableHead>Total de Consultas</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {filteredPatients?.map((patient, index) => (
+                    <TableRow key={index}>
+                      <TableCell>{(patient[1] as Patient).profile?.name}</TableCell>
+                      <TableCell>{(patient[1] as Patient).profile?.email}</TableCell>
+                      <TableCell>{(patient[1] as Patient).profile?.cellphone}</TableCell>
+                      <TableCell>{(patient[1] as Patient).profile?.cpf}</TableCell>
+                      <TableCell>
+                        { 
+                          (patient[1] as Patient).profile?.lastAppointmentDate
+                            ? format(parseISO((patient[1] as Patient).profile?.lastAppointmentDate), 'dd/MM/yyyy')
+                            : "Ainda não possui consulta confirmada"
+                        }
+                      </TableCell>
+                      <TableCell>{(patient[1] as Patient).profile?.totalOfAppointments}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+            </Table>
+          ) }
+          { filteredPatients?.length === 0 && <p className="mt-5 ml-4">Nenhum paciente encontrado.</p> }
       </div>
     </Layout>
   )
