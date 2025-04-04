37a1a6f45a8479b68f8a11c7f20d40467f4a2465
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-04-04 09:57:53 -0300

    feat: create mocked-data

---
+++ mock-specialists-data.js
@@ -0,0 +1,408 @@
+export const specialistsData = {
+    consultation: {
+        "cardiologia": {
+          "thiago-silva": {
+            "agenda": {
+              "date": [
+                "2025-03-06",
+                "2025-03-08",
+                "2025-03-20",
+                "2025-03-22",
+                "2025-04-03",
+                "2025-04-04",
+                "2025-04-17",
+                "2025-04-19"
+                ],
+              "time": [
+                "07:30",
+                "08:00",
+                "08:30",
+                "09:00",
+                "09:30",
+                "10:00",
+                "10:30",
+                "11:00",
+                "11:30"
+              ]
+            },
+            "specialty": "Cardiologia",
+            "name": "Dr. Thiago Silva",
+            "phone": "(11) 98765-4321",
+            "email": "thiago.silva@example.com",
+            "crm": "123456",
+            "id": 1
+          }
+        },
+        "dermatologia": {
+          "luana-peixoto": {
+            "agenda": {
+              "date": [
+                "2025-03-07",
+                "2025-03-09",
+                "2025-03-12",
+                "2025-03-14",
+                "2025-04-07",
+                "2025-04-19",
+                "2025-04-21",
+                "2025-04-26"
+                ],
+              "time": [
+                "07:30",
+                "08:00",
+                "08:30",
+                "09:00",
+                "09:30",
+                "10:00",
+                "10:30",
+                "11:00",
+                "11:30"
+              ]
+            },
+            "specialty": "Dermatologia",
+            "name": "Dra. Luana Peixoto",
+            "phone": "(11) 93598-4159",
+            "email": "luana.peixoto@example.com",
+            "crm": "234567",
+            "id": 2
+          }
+        },
+        "endocrinologia": {
+          "joão-pedro-lima": {
+            "agenda": {
+              "date": [
+                "2025-03-03",
+                "2025-03-07",
+                "2025-03-12",
+                "2025-03-14",
+                "2025-03-19",
+                "2025-03-21",
+                "2025-03-26",
+                "2025-03-28"
+                ],
+              "time": [
+                "07:30",
+                "08:00",
+                "08:30",
+                "09:00",
+                "09:30",
+                "10:00",
+                "10:30",
+                "11:00",
+                "11:30"
+              ]
+            },
+            "specialty": "Endocrinologia",
+            "name": "Dr. João Pedro Lima",
+            "phone": "(11) 98765-4321",
+            "email": "joão.pedro.lima@example.com",
+            "crm": "345678",
+            "id": 3
+          }
+        },
+        "fonoaudiologia": {
+          "cristina-fernandes": {
+            "agenda": {
+              "date": [
+                "2025-03-15",
+                "2025-03-07",
+                "2025-03-12",
+                "2025-03-14",
+                "2025-03-19",
+                "2025-03-21",
+                "2025-03-26",
+                "2025-03-28"
+                ],
+              "time": [
+                "07:30",
+                "08:00",
+                "08:30",
+                "09:00",
+                "09:30",
+                "10:00",
+                "10:30",
+                "11:00",
+                "11:30"
+              ]
+            },
+            "specialty": "Fonoaudiologia",
+            "name": "Dra. Cristina Fernandes",
+            "phone": "(11) 98765-4321",
+            "email": "cristina.fernandes@example.com",
+            "crm": "456789",
+            "id": 4
+          }
+        },
+        "nutrologia": {
+          "mateus-bonfim": {
+            "agenda": {
+              "date": [
+                "2025-03-06",
+                "2025-03-08",
+                "2025-03-13",
+                "2025-03-15",
+                "2025-03-20",
+                "2025-03-22",
+                "2025-03-27",
+                "2025-03-29"
+                ],
+              "time": [
+                "07:30",
+                "08:00",
+                "08:30",
+                "09:00",
+                "09:30",
+                "10:00",
+                "10:30",
+                "11:00",
+                "11:30"
+              ]
+            },
+            "specialty": "Nutrologia",
+            "name": "Dr. Mateus Bonfim",
+            "phone": "(11) 98455-2142",
+            "email": "mateus.bonfim@example.com",
+            "crm": "567890",
+            "id": 5
+          }
+        },
+        "obstetrícia": {
+          "rodrigo-peixoto": {
+            "agenda": {
+              "date": [
+                "2025-03-07",
+                "2025-03-09",
+                "2025-03-14",
+                "2025-03-16",
+                "2025-03-21",
+                "2025-03-23",
+                "2025-03-28",
+                "2025-03-30"
+                ],
+              "time": [
+                "08:00",
+                "08:30",
+                "09:00",
+                "09:30",
+                "10:00",
+                "10:30",
+                "11:00",
+                "11:30"
+              ]
+            },
+            "specialty": "Obstetrícia",
+            "name": "Dr. Rodrigo Peixoto",
+            "phone": "(11) 99764-4325",
+            "email": "rodrigo.peixoto@example.com",
+            "crm": "678901",
+            "id": 6
+          }
+        },
+        "odontologia": {
+          "lucas-mendes": {
+            "agenda": {
+              "date": [
+                "2025-03-08",
+                "2025-03-10",
+                "2025-03-15",
+                "2025-03-17",
+                "2025-03-22",
+                "2025-03-24",
+                "2025-03-29",
+                "2025-03-31"
+                ],
+              "time": [
+                "08:00",
+                "08:30",
+                "09:00",
+                "09:30",
+                "10:00",
+                "10:30",
+                "11:00",
+                "11:30"
+              ]
+            },
+            "specialty": "Odontologia",
+            "name": "Dr. Lucas Mendes",
+            "phone": "(11) 98865-3478",
+            "email": "lucas.mendes@example.com",
+            "crm": "789012",
+            "id": 7
+          }
+        },
+        "odontopediatria": {
+          "julia-lemos": {
+            "agenda": {
+              "date": [
+                "2025-03-09",
+                "2025-03-11",
+                "2025-03-16",
+                "2025-03-18",
+                "2025-03-23",
+                "2025-03-25",
+                "2025-03-30",
+                "2025-04-01"
+                ],
+              "time": [
+                "08:15",
+                "08:45",
+                "09:15",
+                "09:45",
+                "10:15",
+                "10:45",
+                "11:15",
+                "11:45"
+              ]
+            },
+            "specialty": "Odontopediatria",
+            "name": "Dra. Julia Lemos",
+            "phone": "(11) 98765-1154",
+            "email": "julia.lemos@example.com",
+            "crm": "890123",
+            "id": 8
+          }
+        },
+        "oftalmologia": {
+          "marina-castro": {
+            "agenda": {
+              "date": [
+                "2025-03-10",
+                "2025-03-12",
+                "2025-03-17",
+                "2025-03-19",
+                "2025-03-24",
+                "2025-03-26",
+                "2025-03-31",
+                "2025-04-02"
+                ],
+              "time": [
+                "08:15",
+                "08:45",
+                "09:15",
+                "09:45",
+                "10:15",
+                "10:45",
+                "11:15",
+                "11:45"
+              ]
+            },
+            "specialty": "Oftalmologia",
+            "name": "Dra. Marina Castro",
+            "phone": "(11) 98784-9813",
+            "email": "marina.castro@example.com",
+            "crm": "901234",
+            "id": 9
+          }
+        },
+        "ortopedia": {
+          "marcos-trindade": {
+            "agenda": {
+              "date": [
+                "2025-03-11",
+                "2025-03-13",
+                "2025-03-18",
+                "2025-03-20",
+                "2025-03-25",
+                "2025-03-27",
+                "2025-04-01",
+                "2025-04-03"
+                ],
+              "time": [
+                "08:15",
+                "08:45",
+                "09:15",
+                "09:45",
+                "10:15",
+                "10:45",
+                "11:15",
+                "11:45"
+              ]
+            },
+            "specialty": "Ortopedia",
+            "name": "Dr. Marcos Trindade",
+            "phone": "(11) 98784-9813",
+            "email": "marcos.trindade@example.com",
+            "crm": "012345",
+            "id": 10
+          }
+        },
+        "pediatria": {
+          "letícia-carvalho": {
+            "agenda": {
+              "date": [
+                "2025-03-12",
+                "2025-03-14",
+                "2025-03-19",
+                "2025-03-21",
+                "2025-03-26",
+                "2025-03-28",
+                "2025-04-02",
+                "2025-04-04"
+                ],
+              "time": [
+                "08:00",
+                "08:30",
+                "09:00",
+                "09:30",
+                "10:00",
+                "10:30",
+                "11:00",
+                "11:30"
+              ]
+            },
+            "specialty": "Pediatria",
+            "name": "Dra. Letícia Carvalho",
+            "phone": "(11) 963124-6482",
+            "email": "leticia.carvalho@example.com",
+            "crm": "123456",
+            "id": 11
+          }
+        },
+        "psicologia": {
+          "bruna-souza": {
+            "agenda": {
+              "date": [
+                "2025-03-13",
+                "2025-03-15",
+                "2025-03-20",
+                "2025-03-22",
+                "2025-03-27",
+                "2025-03-29",
+                "2025-04-03",
+                "2025-04-03"
+                ],
+              "time": [
+                "07:00",
+                "07:30",
+                "08:00",
+                "08:30",
+                "09:00",
+                "09:30",
+                "10:00",
+                "10:30"
+              ]
+            },
+            "specialty": "Psicologia",
+            "name": "Dra. Bruna Souza",
+            "phone": "(11) 964378-5597",
+            "email": "bruna.souza@example.com",
+            "crm": "234567",
+            "id": 12
+          }
+        }
+    }
+}
+
+export const specialties = [
+  "odontopediatria",
+  "oftalmologia",
+  "ortopedia",
+  "pediatria",
+  "psicologia",
+  "cardiologia",
+  "nutrologia",
+  "endocrinologia",
+  "dermatologia",
+  "odontologia",
+  "fonoaudiologia",
+  "obstetricia",
+]
