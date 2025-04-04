b710e2a67c896e6f3a50fd5dff0e185b5706339d
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-04-04 10:03:18 -0300

    refactor: remove components and pages

--- utils/functions/formatSpecialityAndName.ts
+++
@@ -1,7 +0,0 @@
-export function formatSpecialityAndName(selectedEspeciality: string[]) {
-    const removeTitleName: string = selectedEspeciality[0]?.substring(selectedEspeciality[0]?.indexOf(' ') + 1)
-    const formattedName: string = removeTitleName?.toLowerCase().replaceAll(' ', '-');
-    const formattedespeciality: string = selectedEspeciality[1]?.toLowerCase();
-
-    return { formattedName, formattedespeciality }
-}
